4f9dad438066dfd44d542e3aa83c9bd959bde0b9
Bert Pareyn <pareyn.bert@gmail.com>
2015-01-30 12:02:27 +0000

    Add 'gh' to the data passed to the calendar partial in the util tests

--- tests/qunit/tests/js/api.util.js
+++ tests/qunit/tests/js/api.util.js
@@ -446,10 +446,11 @@ require(['gh.core', 'gh.api.tests'], function(gh, testAPI) {
     QUnit.test('renderTemplate - Partials', function(assert) {
         // Verify that a partial can be used to render a template
         // Add a template to the page
-        $('body').append('<script id="qunit-template-partial" type="text/template"><%= _.partial(\'calendar\', {\'data\': data}) %></script>');
+        $('body').append('<script id="qunit-template-partial" type="text/template"><%= _.partial(\'calendar\', {\'data\': data, \'gh\': gh}) %></script>');
         // Create the data to use in the template
         var data = {
-            'data': null
+            'data': null,
+            'gh': gh
         };
         // Add a target container to the page
         $('body').append('<div id="qunit-template-partial-target" style="display: none;"></div>');
