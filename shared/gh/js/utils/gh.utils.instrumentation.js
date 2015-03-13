e2706b8a5994c50dd26355f1df57784cb9c8b94e
Bert Pareyn <pareyn.bert@gmail.com>
2015-03-13 14:35:21 +0000

    Expand the Segment.io code and fix JS style issues

--- shared/gh/js/utils/gh.utils.instrumentation.js
+++ shared/gh/js/utils/gh.utils.instrumentation.js
@@ -17,10 +17,41 @@ define(['exports'], function(exports) {
 
     var setUpInstrumentation = exports.setUpInstrumentation = function(me, callback) {
         // Load the segment.io JavaScript snippet
-        !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1";
-        analytics.load("GNOd0G9NERQiZDXTpTaNiu1f80nc1iP3");
-        analytics.page();
-        }}();
+        (function() {
+            var analytics = window.analytics = window.analytics || [];
+            if (!analytics.initialize)
+                if (analytics.invoked) {
+                    if (window.console && console.error) {
+                        console.error("Segment snippet included twice.");
+                    }
+                } else {
+                    analytics.invoked = !0;
+                    analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "group", "track", "ready", "alias", "page", "once", "off", "on"];
+                    analytics.factory = function(t) {
+                        return function() {
+                            var e = Array.prototype.slice.call(arguments);
+                            e.unshift(t);
+                            analytics.push(e);
+                            return analytics;
+                        };
+                    };
+                    for (var t = 0; t < analytics.methods.length; t++) {
+                        var e = analytics.methods[t];
+                        analytics[e] = analytics.factory(e);
+                    }
+                    analytics.load = function(t) {
+                        var e = document.createElement("script");
+                        e.type = "text/javascript";
+                        e.async = !0;
+                        e.src = ("https:" === document.location.protocol ? "https://" : "http://") + "cdn.segment.com/analytics.js/v1/" + t + "/analytics.min.js";
+                        var n = document.getElementsByTagName("script")[0];
+                        n.parentNode.insertBefore(e, n);
+                    };
+                    analytics.SNIPPET_VERSION = "3.0.1";
+                    analytics.load("GNOd0G9NERQiZDXTpTaNiu1f80nc1iP3");
+                    analytics.page();
+                }
+        }());
 
         if (!me.anon) {
             analytics.identify(me.id, {
