113a24fdcf6db720c58291b8c7e168b6ff801374
Bert Pareyn <pareyn.bert@gmail.com>
2015-03-18 12:38:08 +0000

    Upload Travis CI release artifacts when build succeeds on master

--- etc/scripts/travis-upload.sh
+++ etc/scripts/travis-upload.sh
@@ -14,7 +14,7 @@ EXPECTED_PULL_REQUEST=false
 
 function package_and_upload {
     rm -rf target
-    bin/package -su --upload-bucket=grasshopper-releases --upload-region=eu-west-1
+    bin/package -su --upload-bucket=grasshopper-ui-releases --upload-region=eu-west-1
 }
 
 if [[ "$TRAVIS_REPO_SLUG" == "$EXPECTED_REPOSITORY" && "$TRAVIS_BRANCH" == "$EXPECTED_BRANCH" && "$TRAVIS_PULL_REQUEST" == "$EXPECTED_PULL_REQUEST" ]]; then
