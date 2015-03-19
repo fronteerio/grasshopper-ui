d0000d74c6035ba8bf8b8576dcad88233c8a844b
Bert Pareyn <pareyn.bert@gmail.com>
2015-03-19 10:31:48 +0000

    Add secure environment variables for AWS upload on Travis

--- etc/scripts/travis-upload.sh
+++ etc/scripts/travis-upload.sh
@@ -8,13 +8,13 @@
 #   * The build was not performed on a pull request to the master branch
 ##
 
-EXPECTED_REPOSITORY="CUL-DigitalServices/grasshoppper-ui"
+EXPECTED_REPOSITORY="CUL-DigitalServices/grasshopper-ui"
 EXPECTED_BRANCH="master"
 EXPECTED_PULL_REQUEST=false
 
 function package_and_upload {
-    rm -rf target
-    bin/package -su --upload-bucket=grasshopper-ui-releases --upload-region=eu-west-1
+    rm -rf ./target
+    ./bin/package -su --upload-bucket=grasshopper-ui-releases --upload-region=eu-west-1
 }
 
 if [[ "$TRAVIS_REPO_SLUG" == "$EXPECTED_REPOSITORY" && "$TRAVIS_BRANCH" == "$EXPECTED_BRANCH" && "$TRAVIS_PULL_REQUEST" == "$EXPECTED_PULL_REQUEST" ]]; then
